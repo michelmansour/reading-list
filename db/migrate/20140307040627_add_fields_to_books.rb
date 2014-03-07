050d7d0a4472b5a192ae9121d992af5bdfb92d1a
Michel Mansour <michel.mansour@gmail.com>
2014-03-06 23:19:03 -0500

    removed the errant commas

--- db/migrate/20140307040627_add_fields_to_books.rb
+++ db/migrate/20140307040627_add_fields_to_books.rb
@@ -1,14 +1,14 @@
 class AddFieldsToBooks < ActiveRecord::Migration
   def change
-    add_column :books, :publication_date, :date,
-    add_column :books, :isbn, :string,
-    add_column :books, :language, :string,
-    add_column :books, :summary, :text,
-    add_column :books, :title_orig, :string,
-    add_column :books, :read_status, :integer,
-    add_column :books, :total_pages, :integer,
-    add_column :books, :pages_read, :integer,
-    add_column :books, :rating, :integer,
+    add_column :books, :publication_date, :date
+    add_column :books, :isbn, :string
+    add_column :books, :language, :string
+    add_column :books, :summary, :text
+    add_column :books, :title_orig, :string
+    add_column :books, :read_status, :integer
+    add_column :books, :total_pages, :integer
+    add_column :books, :pages_read, :integer
+    add_column :books, :rating, :integer
     add_column :books, :completed, :date
   end
 end
