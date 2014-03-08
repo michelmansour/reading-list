c509bace1822f04583e62b3335cfa3b01e19e025
Michel Mansour <michel.mansour@gmail.com>
2014-03-07 20:43:22 -0500

    added summary to book pages

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -46,6 +46,6 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.require(:book).permit(:title, :review)
+    params.require(:book).permit(:title, :review, :summary)
   end
 end
